8644f73994730546835534e51fd9a3b6cffa3826
Serj <swordie.ser@gmail.com>
2021-02-27 20:41:16 +0300

    in progress

--- homework1/geometry.h
+++ homework1/geometry.h
@@ -1,8 +1,82 @@
-//
-// Created by komra on 27.02.2021.
-//
-
 #ifndef IS_2020_PROG_2_SEM_GEOMETRY_H
 #define IS_2020_PROG_2_SEM_GEOMETRY_H
 
-#endif //IS_2020_PROG_2_SEM_GEOMETRY_H
+#include <vector>
+#include "math.h"
+
+using namespace std;
+
+class Point {
+private:
+    int x;
+    int y;
+public:
+    Point() {
+        this->x = 0;
+        this->y = 0;
+    }
+
+    Point(int x, int y) {
+        this->x = x;
+        this->y = y;
+    }
+
+    Point(const Point &point) {
+        this->x = point.x;
+        this->y = point.y;
+    }
+
+    int getX() const {
+        return this->x;
+    }
+
+    int getY() const {
+        return this->y;
+    }
+};
+
+class PolygonalChain : public Point {
+private:
+    vector<Point> points;
+    int n;
+public:
+    PolygonalChain() {
+        this->n = 0;
+    }
+
+    PolygonalChain(int n, Point points[]) {
+        this->n = n;
+        for (int i = 0; i < n; i++) {
+            this->points.push_back(points[i]);
+        }
+    }
+
+    PolygonalChain(const PolygonalChain &pc) : Point(pc) {
+        this->n = pc.n;
+        this->points = pc.points;
+    }
+
+    int getN() const {
+        return this->n;
+    }
+
+    Point getPoint(int k) const {
+        return this->points.at(k);
+    }
+
+    static double range(const Point &p1, const Point &p2) {
+        double distance = sqrt(pow(p1.getX() - p2.getX(), 2) + pow(p1.getY() - p2.getY(), 2));
+        return distance;
+    }
+
+    virtual double perimeter() const {
+        double p = 0;
+        for (int i = 0; i < this->n - 1; i++) {
+            p += range(this->getPoint(i), this->getPoint(i+1));
+        }
+        return p;
+    }
+};
+
+
+#endif
--- homework1/main.cpp
+++ homework1/main.cpp
@@ -6,27 +6,27 @@ using namespace std;
 
 const double EPS = 1e-9;
 
-//void PointCheck() {
-//    const Point p1;
-//    assert(p1.getX() == 0 && p1.getY() == 0);
-//    const Point p2(3, 4);
-//    assert(p2.getX() == 3 && p2.getY() == 4);
-//}
-//
-//void InheritancePolygonalChainCheck(const PolygonalChain *pc, const PolygonalChain *cpc) {
-//    assert(pc->perimeter() == 9);
-//    assert(cpc->perimeter() == 12);
-//}
-//
-//void PolygonsCheck() {
-//    Point *a = new Point[3]{Point(0, 0), Point(3, 4), Point(3, 0)};
-//    Point *b = new Point[4]{Point(0, 0), Point(1, 4), Point(3, 4), Point(3, 0)};
+void PointCheck() {
+    const Point p1;
+    assert(p1.getX() == 0 && p1.getY() == 0);
+    const Point p2(3, 4);
+    assert(p2.getX() == 3 && p2.getY() == 4);
+}
+
+void InheritancePolygonalChainCheck(const PolygonalChain *pc, const PolygonalChain *cpc) {
+    assert(pc->perimeter() == 9);
+    assert(cpc->perimeter() == 12);
+}
+
+void PolygonsCheck() {
+    Point *a = new Point[3]{Point(0, 0), Point(3, 4), Point(3, 0)};
+    Point *b = new Point[4]{Point(0, 0), Point(1, 4), Point(3, 4), Point(3, 0)};
 //    const Polygon p(3, a);
 //    const Triangle t(3, a);
 //    const Trapezoid tr(4, b);
-//    const PolygonalChain pc(3, a);
-//    assert(pc.getN() == 3 && pc.getPoint(1).getX() == 3 && pc.getPoint(1).getY() == 4);
-//    assert(pc.perimeter() == 9);
+    const PolygonalChain pc(3, a);
+    assert(pc.getN() == 3 && pc.getPoint(1).getX() == 3 && pc.getPoint(1).getY() == 4);
+    assert(pc.perimeter() == 9);
 //    const ClosedPolygonalChain cpc(3, a);
 //    a[1] = Point();
 //    assert(cpc.getN() == 3 && cpc.getPoint(1).getX() == 3 && cpc.getPoint(1).getY() == 4);
@@ -46,9 +46,9 @@ const double EPS = 1e-9;
 //    Trapezoid trCopy(*trap);
 //    delete trap;
 //    assert(abs(trCopy.height() - 4) < EPS);
-//}
+}
 
 int main() {
-//    PointCheck();
-//    PolygonsCheck();
+    PointCheck();
+    PolygonsCheck();
 }
